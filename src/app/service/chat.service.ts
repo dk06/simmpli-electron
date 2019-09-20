33556c95fdf0cf5c2a214cd80b234fa999e9f80c
deepak <dk.kumar06@gmail.com>
2019-09-20 15:09:18 +0530

    update code

--- src/app/service/chat.service.ts
+++ src/app/service/chat.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ChatService {
 
-  private url = 'http://localhost:3000?token=abc';
+  private url = 'https://chat-app-deepak.herokuapp.com/?token=abc';
   private socket;
 
   socketId: any = null;
